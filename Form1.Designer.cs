f54377c6a3cd143717b560695bbedac24200a269
Phene <Phene@DESKTOP-MJOLQL9>
2022-04-10 19:21:17 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Bad
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Apps and Features";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(85, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Power Options";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(85, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Event Viewer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(85, 123);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "System";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(85, 152);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(159, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Device Manager";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(85, 181);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(159, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Network Connections";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(85, 210);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(159, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Disk Management";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(85, 239);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(159, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Computer Management";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(85, 268);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(159, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Windows Terminal";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(85, 297);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(159, 23);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Windows Terminal (Admin)";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(85, 326);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(159, 23);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "Task Manager";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(85, 355);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(159, 23);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "Settings";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(85, 384);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(159, 23);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "File Explorer";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(85, 413);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(159, 23);
+            this.button15.TabIndex = 15;
+            this.button15.Text = "Run";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(85, 442);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(159, 23);
+            this.button16.TabIndex = 16;
+            this.button16.Text = "Sign out";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(85, 471);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(159, 23);
+            this.button17.TabIndex = 17;
+            this.button17.Text = "Shut down";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(85, 500);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(159, 23);
+            this.button18.TabIndex = 18;
+            this.button18.Text = "Restart";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(338, 583);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bad Win+X menu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+using System;
+using System.Diagnostics;
+namespace Bad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var url = "ms-settings:appsfeatures";
+            var psi = new ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = url;
+            Process.Start(psi);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var url = "ms-settings:powersleep";
+            var psi = new ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = url;
+            Process.Start(psi);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process eview = new Process();
+            eview.StartInfo.FileName = "mmc.exe";
+            eview.StartInfo.Arguments = "eventvwr.msc";
+            eview.StartInfo.WorkingDirectory = @"c:\windows\system32";
+            eview.EnableRaisingEvents = true;
+            eview.Start();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var url = "ms-settings:about";
+            var psi = new ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = url;
+            Process.Start(psi);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Process devmgr = new Process();
+            devmgr.StartInfo.FileName = "mmc.exe";
+            devmgr.StartInfo.Arguments = "devmgmt.msc";
+            devmgr.StartInfo.WorkingDirectory = @"c:\windows\system32";
+            devmgr.EnableRaisingEvents = true;
+            devmgr.Start();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var url = "ms-settings:network";
+            var psi = new ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = url;
+            Process.Start(psi);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Process dismgr = new Process();
+            dismgr.StartInfo.FileName = "mmc.exe";
+            dismgr.StartInfo.Arguments = "diskmgmt.msc";
+            dismgr.StartInfo.WorkingDirectory = @"c:\windows\system32";
+            dismgr.EnableRaisingEvents = true;
+            dismgr.Start();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Process compmgr = new Process();
+            compmgr.StartInfo.FileName = "mmc.exe";
+            compmgr.StartInfo.Arguments = "compmgmt.msc";
+            compmgr.StartInfo.WorkingDirectory = @"c:\windows\system32";
+            compmgr.EnableRaisingEvents = true;
+            compmgr.Start();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            
+            Process terminal = new Process();
+            terminal.StartInfo.FileName = @"c:\windows\explorer.exe";
+            terminal.StartInfo.Arguments = @"C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.11.3471.0_x64__8wekyb3d8bbwe\wt.exe";
+            terminal.EnableRaisingEvents = true;
+            terminal.Start();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Process terminaladmin = new Process();
+            terminaladmin.StartInfo.FileName = "wt.exe";
+            terminaladmin.EnableRaisingEvents = true;
+            terminaladmin.Start();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Process taskmgr = new Process();
+            taskmgr.StartInfo.FileName = "Taskmgr.exe";
+            taskmgr.StartInfo.Arguments = "/4";
+            taskmgr.StartInfo.WorkingDirectory = @"c:\windows\system32";
+            taskmgr.EnableRaisingEvents = true;
+            taskmgr.Start();
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            var url = "ms-settings:";
+            var psi = new ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = url;
+            Process.Start(psi);
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Process exp = new Process();
+            exp.StartInfo.FileName = "explorer.exe";
+            exp.StartInfo.Arguments = "/Embedding";
+            exp.StartInfo.WorkingDirectory = @"c:\windows\";
+            exp.EnableRaisingEvents = true;
+            exp.Start();
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Process rund = new Process();
+            rund.StartInfo.FileName = "explorer.exe";
+            rund.StartInfo.Arguments = "Shell:::{2559a1f3-21d7-11d4-bdaf-00c04f60b9f0}";
+            rund.StartInfo.WorkingDirectory = @"c:\windows\";
+            rund.EnableRaisingEvents = true;
+            rund.Start();
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Process logout = new Process();
+            logout.StartInfo.FileName = "shutdown.exe";
+            logout.StartInfo.Arguments = "/l /t 0";
+            logout.StartInfo.WorkingDirectory = @"c:\windows\system32";
+            logout.EnableRaisingEvents = true;
+            logout.Start();
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            Process sdown = new Process();
+            sdown.StartInfo.FileName = "shutdown.exe";
+            sdown.StartInfo.Arguments = "/s /t 0";
+            sdown.StartInfo.WorkingDirectory = @"c:\windows\system32";
+            sdown.EnableRaisingEvents = true;
+            sdown.Start();
+
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            Process rstrt = new Process();
+            rstrt.StartInfo.FileName = "shutdown.exe";
+            rstrt.StartInfo.Arguments = "/r /t 0";
+            rstrt.StartInfo.WorkingDirectory = @"c:\windows\system32";
+            rstrt.EnableRaisingEvents = true;
+            rstrt.Start();
+
+        }
+    }
+}
